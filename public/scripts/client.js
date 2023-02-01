245cdf1c5ebdf9ddd44399aba7ad5a74c259685d
fayesadev <fayedumbrigue@gmail.com>
2023-02-01 23:19:11 +0000

    Implemenet renderTweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,53 +3,67 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
-const createTweetElement = function(tweet) {
-  const username = tweet.user.name;
-  // const handle = tweet.user.avatars;
-  const avatar = tweet.user.avatars;
-  const content = tweet.content.text;
-  const timeStamp = tweet.created_at;
-
-  const markup = `
-    <article class="tweet">
-      <header class="tweet">
-      <div class="profile">
-        <i class="fa-solid fa-user fa-2xl"></i>
-        <h4 class="username">Elon Musk</h4>
-      </div>
-      </header>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <footer>
-        <p>10 days ago</p>
-        <div class="icons">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </div>`
-
-  return avatar
-  // $(`<article class="tweet">Hello world</article>`);
-}
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+$(document).ready(function() {
+  
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-
-// const renderTweets = function(tweetArr) {
-
-// }
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+  const createTweetElement = function(tweet) {
+    const username = tweet.user.name;
+    const handle = tweet.user.handle;
+    const avatar = tweet.user.avatars;
+    const content = tweet.content.text;
+    const timeStamp = Date (tweet.created_at);
+  
+    const markup = `
+      <article class="tweet">
+        <header class="tweet">
+        <div class="profile">
+          <image src="${avatar}">
+          <h4 class="username">${username}</h4>
+        </div>
+        </header>
+          <p>${content}</p>
+        <footer>
+          <p>${timeStamp}</p>
+          <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </article>`
+  
+    return markup;
+  }
+  
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    }
+  }
+  
+  renderTweets(data);
+})
