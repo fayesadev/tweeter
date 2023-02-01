16c6a9e4c5437f5d833387386f391d8fbf45da04
fayesadev <fayedumbrigue@gmail.com>
2023-02-01 01:17:26 +0000

    Count characters in text area using input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,37 +1,9 @@
 $(document).ready(function() {
 
-  // $('textarea').click(function() {
-  //   console.log("I was clicked!");
-  //   console.log(this);
-  // })
-
-  // $('textarea').blur(function() {
-  //   console.log("I was blurred");
-  //   console.log(this);
-  // })
-
-  // $('textarea').keydown(function() {
-  //   console.log("keydown");
-  //   console.log(this);
-  // })
-
-  // $('textarea').keyup(function() {
-  //   console.log("keyup");
-  //   console.log(this);
-  // })
-
-  // $('textarea').keypress(function () {
-  //   console.log("keypress");
-  //   console.log(this);
-  // })
-
-  // $('textarea').change(function() {
-  //   console.log("change");
-  //   console.log(this);
-  // })
-
-  $('textarea', this).on('input',function() {
-    console.log("input");
-    console.log();
+  $('textarea').on('input', function() {
+    const limit = 140;
+    const charCount = $(this).val().length;
+    const charLeft = limit - charCount;
+    console.log(charLeft);
   })
 });
